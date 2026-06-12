f5751677300d44e20b4b3d25085cb69af76f16be
Aman Singh <aman9628singh@gmail.com>
2026-06-12 17:33:04 +0530

    change product model name in to VendorProduct

--- backend/controllers/user/cartController.js
+++ backend/controllers/user/cartController.js
@@ -1,5 +1,5 @@
 const Cart = require("../../models/Cart");
-const Product = require("../../models/Product");
+const Product = require("../../models/VendorProduct");
 
 // GET user's cart
 exports.getCart = async (req, res) => {
--- backend/controllers/user/reviewController.js
+++ backend/controllers/user/reviewController.js
@@ -1,5 +1,5 @@
 const Review = require("../../models/Review");
-const Product = require("../../models/Product");
+const Product = require("../../models/VendorProduct");
 const User = require("../../models/User");
 
 exports.getPublicReviews = async (req, res) => {
