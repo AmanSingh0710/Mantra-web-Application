1e910ea663c503bbf6f99c01420b6ab3e735507a
Aman Singh <aman9628singh@gmail.com>
2026-06-29 19:58:33 +0530

    ssss

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.BREVO_HOST,
     port: Number(process.env.BREVO_PORT),
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.BREVO_USER,
         pass: process.env.BREVO_PASS
